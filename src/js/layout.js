b0cd0a5b9e75da79fb5a34fc06102f9a1cc23134
Osagras <148169604+Osagras@users.noreply.github.com>
2024-03-23 04:08:45 +0000

    listo

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import { ContactForm } from "./views/contactForm";
+import { EditForm } from "./views/editForm"
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -23,14 +23,15 @@ const Layout = () => {
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/contactForm" element={<ContactForm />} />
+						<Route path="/editForm/:theid" element={<EditForm />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 					<Footer />
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
+
 	);
 };
 
